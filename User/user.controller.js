81c4a6c9933d9ad89258fdb2d8777268a911f16e
Nishant Jain <nishantkumarajain@gmail.com>
2016-04-02 19:47:39 +0000

    Added routes.

--- User/user.controller.js
+++ User/user.controller.js
@@ -1,40 +1,80 @@
 var express = require('express');
 var router = express.Router();
+var _ = require('underscore');
 
 var UserProfile = require('../User/user-profile.model');
 var Board = require('../board/board.model');
 var Team = require('.././models/team');
 
-router.post('/addCard',function(req,res) {
-  try {
-    UserProfile.addCard(req.body.userId,req.body.card,function(err,card) {
-      res.json(card);
-    });
-  }
-  catch(exception) {
-    res.status(500).json(exception);
-  }
-});
 
-router.post('/addBoard',function(req,res) {
-  try {
-    UserProfile.addBoard(req.body.userId,req.body.board,function(err,doc) {
-      res.json(doc);
-    });
-  }
-  catch(exception) {
-    res.status(500).json(exception);
-  }
-});
-
-router.post('/changeUserDetails',function(req,res,next) {
-
-});
-
-router.post('/addTeam',function(req,res,next) {
-  UserProfile.addTeam(req.body.userId,req.body.team,function(err,doc) {
-    res.json(doc);
+router.get('/:id?',function(req,res,next) {
+  var query = {};
+  console.log(req.params);
+  _.extend(query,req.query,{userId:req.params.id});
+  console.log(query);
+  UserProfile.findUserProfile(query,function(err,profile) {
+    try {
+      if(profile)
+        res.status(200).json(profile);
+      else {
+        res.status(404);
+      }
+    }
+    catch(exception) {
+      //TODO: have to code proper error handling mechanism. This is temporary :(
+      res.status(500).json(exception);
+    }
   });
 });
+
+router.get('/:id/board/:boardId',function(req,res,next) {
+
+});
+
+router.delete('/:id',function(req,res,next) {
+  var query = {};
+  _extend(query,req.body,req.params);
+  UserProfile.removeUserProfile(query,function(error,profile) {
+    try {
+      if(profile)
+        // Response to a successful request that won't be returning a body
+        res.status(204).end();
+      else {
+        res.status(404);
+      }
+    }
+    catch(exception) {
+      res.status(500).end();
+    }
+  });
+});
+
+router.post('/',function(req,res,next) {
+  if(req.body) {
+    UserProfile.createUserProfile(req.body,function(error,profile) {
+      if(error)
+        res.status(500).json(error)
+      else {
+        res.json(profile);
+      }
+    });
+  }
+  else {
+    req.status(400).end();
+  }
+});
+
+router.put('/:id',function(req,res,next) {
+  if(req.body) {
+    _.extend(query,req.body,req.params);
+    UserProfile.findOneAndUpdate(query,function(error,profile) {
+      res.json(profile);
+    });
+  }
+  else {
+    res.status(400).end();
+  }
+});
+
 
 module.exports = router;
