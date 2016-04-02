f97d7eba95f3f9d9e74b829606da7d76500304da
Nishant Jain <nishantkumarajain@gmail.com>
2016-04-02 19:46:41 +0000

    Added few more test cases to spec RESTful models

--- User/user-profile.spec.js
+++ User/user-profile.spec.js
@@ -40,33 +40,53 @@ describe('User Model', function() {
     bio: ''
   };
 
+  var user2 = {
+    userId: '5edd40c86762e0fb12000003',
+    username: 'nishantjain95',
+    displayName: 'Nishant Jain 95',
+    email: 'nishantjain95@live.com',
+    initial: 'NJ',
+    avatar: '',
+    bio: ''
+  };
+
   describe('Create a new User',function() {
-    it('should create a new user',function(done) {
+    it('should create a two new users',function(done) {
       userProfile.createUserProfile(user,function(error,profile){
         should.equal(user.username,profile.username);
-        done();
+        userProfile.createUserProfile(user2,function(error,profile){
+          should.equal(user2.username,profile.username);
+          done();
+        });
       });
     });
   });
 
-  describe('Find a user profile',function() {
-    it('should find a user by userId',function(done) {
-      userProfile.findUserProfile('4edd40c86762e0fb12000003',function(error,profile) {
-        should.equal(user.username,profile.username);
+  describe('Find a user profile by userId',function() {
+    it('should retrive a user profile with the matching userId',function(done) {
+      userProfile.findUserProfile({userId:user.userId},function(error,profile) {
+        should.equal(user.username,profile[0].username);
         done();
       });
     });
   });
 
   describe('Find a user profile by emailId',function() {
-    it('should retrive a user existing with that user id',function(done) {
-      userProfile.findUserProfile({email:'nishantjain94@live.com'},function(error,profile) {
-        console.log('printing profile...');
-        console.log(error,profile);
+    it('should retrive a user existing with matching emailId',function(done) {
+      userProfile.findUserProfile({email:'nishantjain94@live.com',fields: 'username displayName'},function(error,profile) {
+        should.equal(user.username,profile[0].username);
         done();
       });
     });
   });
+
+  describe('Find a user profile which matches the pattern',function() {
+    it('should retrieve all the users matching the pattern with username and displayName',function(done) {
+      userProfile.findUserProfile({username:'_nish',fields:'username displayName'},function(error,profile) {
+        done();
+      });
+    });
+  })
 
   describe('Update a user profile',function(){
     it('should update the user profile',function(done) {
@@ -84,7 +104,6 @@ describe('User Model', function() {
           displayName: 'Nishant Jain'
       };
       userProfile.modifyUserProfile('4edd40c86762e0fb12000003',updatedProfile,function(error,profile) {
-        console.log(error,profile);
         should.equal(profile.displayName,'Nishant Jain');
         done();
       });
@@ -97,7 +116,6 @@ describe('User Model', function() {
         userId: '4edd40c86762e0fb12000004'
       };
       userProfile.modifyUserProfile('4edd40c86762e0fb12000003',updatedProfile,function(error,profile) {
-        console.log(error,profile);
         should.notEqual(profile.userId,'4edd40c86762e0fb12000004');
         done();
       })
